fda214a6bc976a665d017a1bd432297e9fc01f73
talhabayburtlu <talha_bayburtlu2@hotmail.com>
2021-01-08 23:54:30 +0300

    Init commit.

---
+++ src/main/java/com/springboot/EAPSCO/EapscoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.EAPSCO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EapscoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EapscoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/EAPSCO/EapscoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.EAPSCO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EapscoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
